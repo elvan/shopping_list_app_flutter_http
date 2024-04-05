1522c77ad77fb3a35217aa2dcb3d218edc7cbf01
Elvan Hidayat <elvanhd@gmail.com>
2024-04-05 20:30:07 +0700

    Introduce loading indicators for UI feedback
    - Implement a loading spinner during initial item fetch operation
    - Display CircularProgressIndicator while fetching items on startup
    - Enable a sending state with spinner during item submission
    - Disable form buttons while an item is being submitted to the backend
    - Update UI responsively to provide feedback during network operations
    - Ensure user experience is smooth with visual indicators for background tasks

--- lib/widgets/grocery_list.dart
+++ lib/widgets/grocery_list.dart
@@ -15,6 +15,7 @@ class GroceryList extends StatefulWidget {
 
 class _GroceryListState extends State<GroceryList> {
   List<GroceryItem> _groceryItems = [];
+  var _isLoading = true;
 
   @override
   void initState() {
@@ -45,6 +46,7 @@ class _GroceryListState extends State<GroceryList> {
     }
     setState(() {
       _groceryItems = loadedItems;
+      _isLoading = false;
     });
   }
 
@@ -73,6 +75,10 @@ class _GroceryListState extends State<GroceryList> {
   @override
   Widget build(BuildContext context) {
     Widget content = const Center(child: Text('No items added yet.'));
+
+    if (_isLoading) {
+      content = const Center(child: CircularProgressIndicator());
+    }
 
     if (_groceryItems.isNotEmpty) {
       content = ListView.builder(
