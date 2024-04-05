432d7cda1fa45e5fa66d278e1e12194d27b1b9b6
Elvan Hidayat <elvanhd@gmail.com>
2024-04-05 14:07:36 +0700

    Enable dynamic addition of grocery items
    - Modify GroceryList to manage a dynamic list of grocery items
    - Update NewItem form to return a GroceryItem on submission
    - Implement async navigation to NewItem with result handling
    - Add functionality to add new item to grocery list on form submission
    - Ensure the grocery list updates and displays new items correctly
    - Use setState to refresh the list view with the newly added item

--- lib/widgets/new_item.dart
+++ lib/widgets/new_item.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:shopping_list/data/categories.dart';
 import 'package:shopping_list/models/category.dart';
+import 'package:shopping_list/models/grocery_item.dart';
 
 class NewItem extends StatefulWidget {
   const NewItem({super.key});
@@ -20,9 +21,14 @@ class _NewItemState extends State<NewItem> {
   void _saveItem() {
     if (_formKey.currentState!.validate()) {
       _formKey.currentState!.save();
-      print(_enteredName);
-      print(_enteredQuantity);
-      print(_selectedCategory);
+      Navigator.of(context).pop(
+        GroceryItem(
+          id: DateTime.now().toString(),
+          name: _enteredName,
+          quantity: _enteredQuantity,
+          category: _selectedCategory,
+        ),
+      );
     }
   }
 
