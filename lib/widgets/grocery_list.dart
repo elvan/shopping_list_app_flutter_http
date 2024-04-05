d1d77756e1b2e5e4857358d14c9d137b7c16e65f
Elvan Hidayat <elvanhd@gmail.com>
2024-04-05 20:41:48 +0700

    Handle errors during item fetch operation
    - Introduce error handling for the item fetch operation
    - Display an error message when fetching data fails
    - Check for HTTP status codes that indicate a failure
    - Update state with an error message for failed fetch attempts
    - Provide user feedback on network or server errors

--- lib/widgets/grocery_list.dart
+++ lib/widgets/grocery_list.dart
@@ -16,6 +16,7 @@ class GroceryList extends StatefulWidget {
 class _GroceryListState extends State<GroceryList> {
   List<GroceryItem> _groceryItems = [];
   var _isLoading = true;
+  String? _error;
 
   @override
   void initState() {
@@ -28,6 +29,13 @@ class _GroceryListState extends State<GroceryList> {
         'flutter-http-request-e7f20-default-rtdb.asia-southeast1.firebasedatabase.app',
         'shopping-list.json');
     final response = await http.get(url);
+
+    if (response.statusCode >= 400) {
+      setState(() {
+        _error = 'Failed to fetch data. Please try again later.';
+      });
+    }
+
     final Map<String, dynamic> listData = json.decode(response.body);
     final List<GroceryItem> loadedItems = [];
     for (final item in listData.entries) {
@@ -101,6 +109,10 @@ class _GroceryListState extends State<GroceryList> {
           ),
         ),
       );
+    }
+
+    if (_error != null) {
+      content = Center(child: Text(_error!));
     }
 
     return Scaffold(
