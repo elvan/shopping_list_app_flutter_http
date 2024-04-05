1815efcd821b7461d505a8c601aa83cd95c3b8e5
Elvan Hidayat <elvanhd@gmail.com>
2024-04-05 20:21:41 +0700

    Optimize item addition and immediate UI update
    - Adjust addItem method to immediately update UI with new item
    - Restore newItem navigation to return a GroceryItem upon submission
    - Update newItem submission to parse Firebase response for item ID
    - Ensure newItem form returns created item for immediate UI update
    - Include GroceryItem model import in newItem for item construction
    - Remove debug print statements for cleaner code

--- lib/widgets/new_item.dart
+++ lib/widgets/new_item.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:shopping_list/data/categories.dart';
 import 'package:shopping_list/models/category.dart';
+import 'package:shopping_list/models/grocery_item.dart';
 
 class NewItem extends StatefulWidget {
   const NewItem({super.key});
@@ -40,14 +41,20 @@ class _NewItemState extends State<NewItem> {
         ),
       );
 
-      print(response.body);
-      print(response.statusCode);
+      final Map<String, dynamic> resData = json.decode(response.body);
 
       if (!context.mounted) {
         return;
       }
 
-      Navigator.of(context).pop();
+      Navigator.of(context).pop(
+        GroceryItem(
+          id: resData['name'],
+          name: _enteredName,
+          quantity: _enteredQuantity,
+          category: _selectedCategory,
+        ),
+      );
     }
   }
 
