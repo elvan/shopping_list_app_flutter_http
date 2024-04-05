de478efbff4f21a920c2a969094c76e2a245fe0b
Elvan Hidayat <elvanhd@gmail.com>
2024-04-05 19:45:52 +0700

    Add network request to save new grocery items
    - Introduce HTTP POST request to save new items to a remote database
    - Use dart:convert for JSON serialization of item data
    - Add http package to pubspec.yaml for making network requests
    - Encode item name, quantity, and category into JSON for POST request
    - Specify URL for Firebase Realtime Database as the endpoint
    - Set Content-Type header to application/json in the request
    - Comment out Navigator pop to focus on network request functionality

--- lib/widgets/new_item.dart
+++ lib/widgets/new_item.dart
@@ -1,7 +1,9 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 import 'package:shopping_list/data/categories.dart';
 import 'package:shopping_list/models/category.dart';
-import 'package:shopping_list/models/grocery_item.dart';
 
 class NewItem extends StatefulWidget {
   const NewItem({super.key});
@@ -21,14 +23,23 @@ class _NewItemState extends State<NewItem> {
   void _saveItem() {
     if (_formKey.currentState!.validate()) {
       _formKey.currentState!.save();
-      Navigator.of(context).pop(
-        GroceryItem(
-          id: DateTime.now().toString(),
-          name: _enteredName,
-          quantity: _enteredQuantity,
-          category: _selectedCategory,
+      final url = Uri.https(
+          'flutter-http-request-e7f20-default-rtdb.asia-southeast1.firebasedatabase.app',
+          'shopping-list.json');
+      http.post(
+        url,
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: json.encode(
+          {
+            'name': _enteredName,
+            'quantity': _enteredQuantity,
+            'category': _selectedCategory.title,
+          },
         ),
       );
+      // Navigator.of(context).pop();
     }
   }
 
